c74657608fc4c9dfbba46fbcd2ff3cc34fb3ff0a
Gena <thipi1213@live.com>
2024-01-13 16:19:44 -0500

    Initial Commit

---
+++ SugarCounter/SugarCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SugarCounterApp.swift
+//  SugarCounter
+//
+//  Created by User on 2024-01-13.
+//
+
+import SwiftUI
+
+@main
+struct SugarCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
